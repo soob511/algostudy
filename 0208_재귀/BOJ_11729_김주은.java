d9077ba43f67127cf592a60937bdf6f42a802dbe
김주은 <ducky_313@naver.com>
2022-02-08 08:47:12 +0900

    하노이 김주은

---
+++ 0208_재귀/BOJ_11729_김주은.java
@@ -0,0 +1,36 @@
+package com.ssafy.add.d0208;
+
+import java.util.Scanner;
+
+public class boj_11729_하노이탑이동순서 {
+	
+	static int cnt=0;
+	static StringBuilder sb;
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		sb = new StringBuilder();
+		
+		int N = sc.nextInt();
+		
+		hanoi(N, 1, 2, 3);
+
+		System.out.println(cnt + "\n" + sb.toString());
+	}
+	
+
+	public static void hanoi(int N, int from, int step, int to) {
+		
+		if(N==0) return;
+		
+		hanoi(N-1,from,to,step);
+		
+		sb.append(from+" "+to+"\n");
+		++cnt;
+		
+		hanoi(N-1,step,from,to);
+	}
+
+}
