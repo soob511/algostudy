541782c2ece69c19ffd6fd25c169e3a1922497af
전수빈 <asdfls@naver.com>
2022-02-09 22:34:43 +0900

    카드2

---
+++ 0210_스택,큐/BOJ_2164_전수빈.java
@@ -0,0 +1,31 @@
+package queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main_2164_카드2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Queue<Integer>  queue = new LinkedList<>(); //큐 생성
+		int n = sc.nextInt();//갯수입력
+		
+		for(int i=1;i<=n;i++) {
+			queue.offer(i); //1부터 숫자 입력
+		}
+		
+		
+		
+		while(queue.size()>1) {
+			queue.poll();
+		
+			int back = queue.poll();
+			
+			queue.offer(back);
+			
+		}
+		System.out.println(queue.poll());
+	}
+
+}
