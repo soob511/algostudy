e2cda89aa28c4b44c154605e6d2d5b9db090f050
전수빈 <asdfls@naver.com>
2022-02-15 11:51:18 +0000

    Update BOJ_1874_전수빈.java

--- 0215_/BOJ_1874_전수빈.java
+++ 0215_/BOJ_1874_전수빈.java
@@ -17,20 +17,20 @@ public class Main_1874_수열 {
 			int num = sc.nextInt();
 			
 			if(cnt<num) {
-				for(int i=cnt+1;i<=num;i++) {
+				for(int i=cnt+1;i<=num;i++) { //입력한숫자까지 입력
 					stack.push(i);
 					sb.append('+');
 				}
-				cnt = num;
+				cnt = num;// 오름차순 유지
 				
 			}
 			
-			else if(stack.peek() != num) {
+			else if(stack.peek() != num) { 
 				System.out.println("No");
 				return;
 			}
 			
-			stack.pop();
+			stack.pop(); // -
 			sb.append('-');
 		}
 		
