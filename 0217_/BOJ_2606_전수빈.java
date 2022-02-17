a2d228603018561dc1c47b5562b2038cec4737cd
전수빈 <asdfls@naver.com>
2022-02-17 08:40:50 +0000

    바이러스

--- 0217_/BOJ_2606_전수빈.java
+++ 0217_/BOJ_2606_전수빈.java
@@ -12,12 +12,13 @@ public class Main_2606_바이러스 {
 		Scanner sc = new Scanner(System.in);
 		 c = sc.nextInt(); //컴퓨터수
 		 n = sc.nextInt(); //쌍의수
-		map = new int[c+1][c+1];
-		check = new boolean[c+1];
+		map = new int[c+1][c+1]; //인접 행렬
+		check = new boolean[c+1];// 바이러스 체크
+		
 		for(int i=0;i<n;i++) {
 			int a = sc.nextInt(); 
 			int b = sc.nextInt();
-			map[a][b] = map[b][a] =1;
+			map[a][b] = map[b][a] =1;	//2차원 배열애 체크
 		}
 		dfs(1);
 		System.out.println(count);
@@ -25,9 +26,10 @@ public class Main_2606_바이러스 {
 	private static void dfs(int cnt) {
 		// TODO Auto-generated method stub
 		
-		check[cnt] = true;
+		check[cnt] = true; //방문체크
+		
 		for(int i=1;i<=c;i++ ) {
-			if(map[cnt][i]==1 && check[i]==false) {
+			if(map[cnt][i]==1 && check[i]==false) { //간선으로 연결되있음
 				count++;
 				dfs(i);
 			}
