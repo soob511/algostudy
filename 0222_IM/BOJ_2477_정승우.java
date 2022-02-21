2057abc553a6b55cf743a681a7a9c2a3bb167101
정승우 <tmddn1008@naver.com>
2022-02-21 18:20:51 +0000

    참외

---
+++ 0222_IM/BOJ_2477_정승우.java
@@ -0,0 +1,34 @@
+package com.ssafy.day0222;
+
+import java.util.Scanner;
+
+public class BOJ_2477_정승우 {
+
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int K = sc.nextInt();
+		int length[] = new int[6];
+		
+		for (int i=0; i<6; i++) {
+			sc.nextInt();
+			length[i] = sc.nextInt();
+			
+		}
+		
+		int max =0;
+		int idx =0;
+		for (int i=0; i<6; i++) {
+			//연속하는 두 수중 가장 큰 값이 전체의 가로,세로
+			if (max<length[i]*length[(i+1)%6]) {
+				max = length[i]*length[(i+1%6)];
+				idx=i;
+			}
+		}
+		//전체 가로세로에서 3,4번째 뒤 인덱스는 작은 가로세로(작은 가로세로는 면적에 포함
+		//되지 않기때문에 전체 면적에서 작은 면적을 뺌)
+		max = max - length[(idx+3)%6]*length[(idx+4)%6];
+		System.out.println(max*K);
+	}
+
+}
