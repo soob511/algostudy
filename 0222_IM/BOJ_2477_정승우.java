721adf00bdf1a480c32b9fe0360afd8c351b8ee2
이길상 <oat641@gmail.com>
2022-02-24 12:32:51 +0000

    Update BOJ_2477_정승우.java

--- 0222_IM/BOJ_2477_정승우.java
+++ 0222_IM/BOJ_2477_정승우.java
@@ -21,10 +21,12 @@ public class BOJ_2477_정승우 {
 		for (int i=0; i<6; i++) {
 			//연속하는 두 수중 가장 큰 값이 전체의 가로,세로
 			if (max<length[i]*length[(i+1)%6]) {
-				max = length[i]*length[(i+1%6)];
+				max = length[i]*length[((i+1)%6)];
 				idx=i;
 			}
 		}
+		// 160, 30, 60, 20, 100, 50     i = 2, 3
+
 		//전체 가로세로에서 3,4번째 뒤 인덱스는 작은 가로세로(작은 가로세로는 면적에 포함
 		//되지 않기때문에 전체 면적에서 작은 면적을 뺌)
 		max = max - length[(idx+3)%6]*length[(idx+4)%6];
