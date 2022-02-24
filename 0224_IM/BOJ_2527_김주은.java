83e6fdea703926e49dbc7766cdd0609b9a76f355
김주은 <ducky_313@naver.com>
2022-02-24 13:04:24 +0000

    Update BOJ_2527_김주은.java

--- 0224_IM/BOJ_2527_김주은.java
+++ 0224_IM/BOJ_2527_김주은.java
@@ -29,13 +29,16 @@ public class Main_BJ_2527_직사각형_김주은 {
 				sb.append("d").append("\n");
 			}
 			//c인 경우 (우상, 우하, 좌하, 좌상 순서)
-			else if((p1==x2 && q1==y2) || (p1==x2 && y1==y2) || (x1==p2 && y1==q2) || (x1==p2 && q1==y2)) {
+			else if((p1==x2 && q1==y2) || (p1==x2 && y1==q2) || (x1==p1 && y1==q1) || (x1==p2 && q1==y2)) {
 				sb.append("c").append("\n");
 			}
 			//b인 경우 (상, 우, 하, 좌 차례로 접함)
-			else if(q1==y2 || p1==x2 || y1==q2 || x1==p2) {
+			else if((q1==y2 && (x1<p2 || p1>x2)) || (p1==x2 && (q1>y2 || p1>x2)) || (y1==q2 &&(x1<p2 || p1>x2))|| (x1==p2 && (q1>y2 || p1>x2))) {
 				sb.append("b").append("\n");
 			}
+//			else if((q1==y2 && x1<p2 && p1>x2) || (p1==x2 && q1>y2 && p1>x2) || (y1==q2 && x1<p2 && p1>x2)|| (x1==p2 && q1>y2 && p1>x2)) {
+//				sb.append("b").append("\n");
+//			}
 			//a인 경우
 			else {
 				sb.append("a").append("\n");
