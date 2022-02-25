8007fc9e51a95fdf68b0a95c7111ed5448c759b5
Nelfunction <oat641@gmail.com>
2022-02-25 16:46:26 +0900

    개미 수정

--- 0301_IM/BOJ_10158_이길상.java
+++ 0301_IM/BOJ_10158_이길상.java
@@ -1,34 +1,34 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class IM_10_10158_개미 {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] line = br.readLine().split(" ");
-		int W = Integer.parseInt(line[0]), H = Integer.parseInt(line[1]);
 		
-		line = br.readLine().split(" ");
-		int P = Integer.parseInt(line[0]), Q = Integer.parseInt(line[1]);
+		// 공간 크기 입력
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int W = Integer.parseInt(st.nextToken()), H = Integer.parseInt(st.nextToken());
+		
+		// 개미 위치 입력
+		st = new StringTokenizer(br.readLine());
+		int P = Integer.parseInt(st.nextToken()), Q = Integer.parseInt(st.nextToken());
+		
+		// 시간 입력
 		int T = Integer.parseInt(br.readLine());
 		
+		// 연장선 위의 위치
 		P+=T;
 		Q+=T;
-		
-		int i = P/W, j = Q/H;
+
+		// 제한된 공간 안의 위치로 변환
 		int x = P%W, y = Q%H;
+		if((P/W)%2 == 1) x = W - x;
+		if((Q/H)%2 == 1) y = H - y;
 		
-		StringBuilder sb = new StringBuilder();
-		
-		if(i%2 == 0) sb.append(x);
-		else sb.append(W - x);
-		
-		sb.append(" ");
-		
-		if(j%2 == 0) sb.append(y);
-		else sb.append(H - y);
-		
-		System.out.println(sb.toString());
+		// 출력
+		System.out.println(x + " " + y);
 	}
 
 }
