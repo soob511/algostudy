34c0c189ed06215566c4cd88e1d82e7bea105021
김주은 <ducky_313@naver.com>
2022-03-03 12:29:50 +0000

    Update BOJ_2116_김주은.java

--- 0301_IM/BOJ_2116_김주은.java
+++ 0301_IM/BOJ_2116_김주은.java
@@ -23,6 +23,7 @@ public class Main_BJ_2116_주사위쌓기_김주은 {
 		}
 
 		for(int i=0;i<6;i++) {
+			//index 차례대로 가장 바닥에 두면서 총 6번 탐색
 			search(0, dice[0][findTop(i)], 0);
 		}
 		
@@ -47,18 +48,22 @@ public class Main_BJ_2116_주사위쌓기_김주은 {
 		else return 0;
 	}
 	
+	//cnt: 몇번째 탐색, bottomValue : 현재 주사위 가장 바닥 값, sum: 옆면 최댓값을 더한 값
 	static void search(int cnt, int bottomValue, int sum) {
+		//마지막 주사위일 경우에 max 비교
 		if(cnt==N) {
 			if(sum>max) max=sum;
 			return;
 		}		
 		int idx=-1;
+		//현재 바닥값과 동일한 인덱스 탐색
 		for(int i=0;i<6;i++) {
 			if(dice[cnt][i]==bottomValue) {
 				idx=i;
 				break;
 			}
-		}		
+		}
+		//다음 순서, 현재 주사위의 top을 넘김, 현재 주사위의 옆면 최댓값을 더해줌		
 		search(cnt+1, dice[cnt][findTop(idx)], sum+findMax(cnt, idx));
 	}
 
