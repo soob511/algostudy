18c645d598bf92f4f7fc08e2f7e220d990e22c30
김주은 <ducky_313@naver.com>
2022-03-22 12:06:44 +0000

    Update BOJ_9663_김주은.java

--- 0317_브루트포스,최단경로/BOJ_9663_김주은.java
+++ 0317_브루트포스,최단경로/BOJ_9663_김주은.java
@@ -8,7 +8,50 @@ public class Main_BJ_9663_NQueen_김주은 {
 	static int N;
 	static int qcount=0;
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		
+		int[] map = new int[N];
+		
+		search(map,0); //map, 퀸을 놓을 0번째 행부터 탐색
+		
+		System.out.println(qcount);
+		
+	}
+	
+	static void search(int[] map, int i) {
+		
+		if(i>=N) {
+			++qcount;
+			return;
+		}
+		
+		outer: for(int j=0;j<N;j++) {
+			
+			//퀸을 놓을 수 없는 경우 체크
+			for(int t=0;t<i;t++) {
+				//같은 열에 있거나 대각선에 있는 경우
+				if(map[t]==j || i-t == Math.abs(map[t]-j)) {
+					continue outer;
+				}
+			}
+			
+			map[i]=j;
+			search(map, i+1);
+		}
+		return;
+	}
+}
+
+/*
+public class Main_BJ_9663_NQueen_김주은 {
+	
+	static int N;
+	static int qcount=0;
+
+	public static void main(String[] args) throws Exception {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
@@ -40,34 +83,35 @@ public class Main_BJ_9663_NQueen_김주은 {
 		return map;
 	}
 	
-	static boolean search(boolean[][] map, int i) {
+	static void search(boolean[][] map, int i) {
 		
-		if(i>N) {
+		if(i>=N) {
 			++qcount;
-			return true;
+			return;
 		}
 		
-		boolean[][] map2 = arrayCopy(map);
-		boolean noqueen = true; //퀸을 놓지 않는지 체크하기 위한 변수
+		boolean[][] prevmap;
 		
 		for(int j=0;j<N;j++) {
+			
 			if(map[i][j]) continue; //이미 true이면 continue
 			
 			System.out.println(i+":"+j);
 			
-			map2 = setBlocks(map,i,j); //map[i][j]에 퀸을 둘 때의 map
+			prevmap = arrayCopy(map);
 			
-			if(search(map2, i+1)) {
-				
-			}else if(i==0){
-				continue;
+			map = setBlocks(map,i,j); //map[i][j]에 퀸을 둘 때의 map
+			
+			search(map, i+1);
+
+			if(i==0) {
+				map = new boolean[N][N];
+			}else {
+				map = arrayCopy(prevmap);
 			}
-			else{
-				return false;
-			}
-				
 		}
-		return false;
+		
+		return; //행에 아무 퀸도 놓지 않은 경우 리턴
 		
 	}
 	
@@ -80,3 +124,5 @@ public class Main_BJ_9663_NQueen_김주은 {
 	}
 
 }
+*/
+
