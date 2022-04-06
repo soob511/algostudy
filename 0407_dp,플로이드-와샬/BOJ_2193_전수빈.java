dbde87b5e123c8a937b121cea503cca8105131ce
전수빈 <asdfls@naver.com>
2022-04-06 22:15:25 +0900

    이친수

---
+++ 0407_dp,플로이드-와샬/BOJ_2193_전수빈.java
@@ -0,0 +1,25 @@
+package dp;
+
+import java.util.Scanner;
+
+public class Main_2193_이친수 {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int n = sc.nextInt();
+		   long [] dp = new long[n+1];
+	        
+	        dp[0] = 0;
+	        dp[1] = 1;
+	 
+	        for(int i=2; i<=n; i++) {
+	        	dp[i] = dp[i-1]+dp[i-2];
+	        	
+	        }
+	        
+	        System.out.println(dp[n]);
+	    }
+
+
+
+}
