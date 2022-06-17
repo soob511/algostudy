869f04cb9859500188b37b1a27a40fa2ad4cc116
전수빈 <asdfls@naver.com>
2022-06-17 03:57:23 +0000

    Update PS_타겟넘버_전수빈.java

--- 0601/PS_타겟넘버_전수빈.java
+++ 0601/PS_타겟넘버_전수빈.java
@@ -1,10 +1,24 @@
-package jobpair;
+class Solution {
+    static int count = 0;
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
 
-public class baby {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+        dfs(0,numbers,target,0);
+        
+        answer = count;
+        return answer;
+    }
+    
+    public void dfs(int index, int[] numbers, int target, int sum){
+        if(index == numbers.length) { 
+            if (sum == target) {
+                count++;
+            }
+            return;
+        }
+        
+        dfs(index+1, numbers,target,sum+numbers[index]); //더하는 경우
+        dfs(index+1, numbers,target,sum-numbers[index]); //빼는 경우
+        
+    }
 }
