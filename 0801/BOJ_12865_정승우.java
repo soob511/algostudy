fd0a4d271b3ca4d5ae42ca2aa63aa07a34a02ed9
정승우 <tmddn1008@naver.com>
2022-08-23 17:22:52 +0900

    Update BOJ_12865_정승우.java

--- 0801/BOJ_12865_정승우.java
+++ 0801/BOJ_12865_정승우.java
@@ -1,59 +1,41 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
 public class BOJ_12865_정승우 {
-
-    static Integer[][] dp;
-    static int[] W; // 무게
-    static int[] V; // 가치
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[N+1][K+1];
+        int[] w = new int[N+1];
+        int[] v = new int[N+1];
 
-        W = new int[N];
-        V = new int[N];
-
-        dp = new Integer[N][K + 1];
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            W[i] = Integer.parseInt(st.nextToken());
-            V[i] = Integer.parseInt(st.nextToken());
+        for(int i=1; i<=N; i++) {
+            st = new StringTokenizer(br.readLine());
+            w[i] = Integer.parseInt(st.nextToken());
+            v[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(knapsack(N - 1, K));
-
-    }
-
-    static int knapsack(int i, int k) {
-        
-        if (i < 0)
-            return 0;
-
-        // 탐색하지 않은 곳
-        if (dp[i][k] == null) {
-
-            // 현재 물건을 추가로 못담는 경우
-            if(W[i] > k) {
-                dp[i][k] = knapsack(i - 1, k);
-            }
-            // 현재 물건을 담을 수 있는 경우
-            else {
-                // 이전 i값과 비교하여 큰 값을 저장
-                dp[i][k] = Math.max(knapsack(i - 1, k), knapsack(i - 1, k - W[i]) + V[i]);
+        for (int i=1; i<=N; i++) {
+            for (int j=1; j<=K; j++) {
+                //이전의 값을 저장
+                dp[i][j] = dp[i-1][j];
+                //담을수 있는 무게에서 자신의 무게를 빼서 남는 무게가 있다면
+                if(j - w[i]>=0) {
+                    //이전에 구한 가치와 자신의 무게를 뺐을때 담을수 있는 가치와 자신의 가치를 더해서 더 큰값으로 dp[i[j] 값을 초기화
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-w[i]]+v[i]);
+                }
             }
         }
-        return dp[i][k];
-    }
 
+
+        System.out.println(dp[N][K]);
+    }
 
 }
