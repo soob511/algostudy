cea2f219b815c34d346a11f94216692bd56b7bcb
tmddn1008 <tmddn1008@naver.com>
2022-11-30 17:02:06 +0900

    정승우

---
+++ 1130/BOJ_2751_정승우.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class BOJ_2751_정승우 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(list);
+
+        for(int value : list) {
+            sb.append(value).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
