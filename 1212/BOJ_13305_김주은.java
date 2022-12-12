dde3ae66e78aebc39581e97d5cb6b339057b3604
JooEunKim <44815092+joooeunkim@users.noreply.github.com>
2022-12-13 01:42:53 +0900

    주유소

--- 1212/BOJ_13305_김주은.java
+++ 1212/BOJ_13305_김주은.java
@@ -17,16 +17,28 @@ public class Main_BJ_13305_주유소_김주은 {
 		for(int i=0;i<N-1;i++) {
 			dist[i] = Integer.parseInt(st.nextToken());
 		}
-
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
-		
+	
 		st = new StringTokenizer(br.readLine());
 		for(int i=0;i<N;i++) {
 			price[i] = Integer.parseInt(st.nextToken());
-			pq.add(price[i]);
 		}
 		
 		
+		int sum_price = 0;
+		int sum_dist = 0;
+		int min_price = price[N-2];
+		
+		for(int i=N-2;i>=0;i--) {
+			
+			sum_dist += dist[i];
+			
+			if(price[i]<=min_price) {
+				sum_price = sum_dist * price[i];
+			} else {
+				sum_price += dist[i]*price[i];
+			}
+		}
+		
+		System.out.println(sum_price);
 	}
-
 }
