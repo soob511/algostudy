2ee51e9ae2fb3719004f200b20e8428417b3146a
JooEunKim <44815092+joooeunkim@users.noreply.github.com>
2022-12-15 01:46:19 +0900

    구간합구하기

--- 1214/BOJ_2042_김주은.java
+++ 1214/BOJ_2042_김주은.java
@@ -2,8 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main_BJ_2042_구간합구하기_김주은 {
-	
-	static int N,M,K;
+
 	static long[] input, tree;
 
 	public static void main(String[] args) throws Exception {
@@ -12,11 +11,16 @@ public class Main_BJ_2042_구간합구하기_김주은 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
 		
 		input = new long[N+1]; // 값 입력 배열
+		
+//		int k = (int) Math.ceil(Math.log(N)/Math.log(2)) + 1;
+//		int size = (int) Math.pow(2,k);
+//		
+//		tree = new long[size]; //세그먼트 트리 배열
 		
 		int size = 1;
 		while(size<N) {
@@ -35,13 +39,15 @@ public class Main_BJ_2042_구간합구하기_김주은 {
 			st = new StringTokenizer(br.readLine());
 			
 			int a = Integer.parseInt(st.nextToken());
-			long b = Long.parseLong(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 			long c = Long.parseLong(st.nextToken());
 			
 			if(a==1) { //숫자 변경
-				updateTree(1,1,N,b,c-input[b]);
+				long diff = c - input[b];
+				input[b]=c;
+				updateTree(1,1,N,b,diff);
 			} else if(a==2) { //구간 합 구하기
-				sb.append(query(1,1,N,b,c)).append("\n");
+				sb.append(query(1,1,N,b,(int)c)).append("\n");
 			}
 		}
 		
@@ -49,7 +55,7 @@ public class Main_BJ_2042_구간합구하기_김주은 {
 		
 	}
 
-	private static long query(int node, long l, long r, long ql, long qr) {
+	private static long query(int node, int l, int r, int ql, int qr) {
 		if(qr<l || r<ql) {
 			return 0;
 		}
@@ -58,31 +64,35 @@ public class Main_BJ_2042_구간합구하기_김주은 {
 			return tree[node];
 		}
 		
-		long mid = (l+r)/2;
+		int mid = (l+r)/2;
 		
 		return query(2*node, l, mid, ql,qr) + query(2*node+1,mid+1,r,ql,qr);
 	}
 
-	private static long updateTree(int node, long l, long r, long idx, long diff) {
+	private static void updateTree(int node, int l, int r, int idx, long diff) {
+		
 		if(idx<l || r<idx) {
-			return tree[node];
+			return;
 		}
+		
+		tree[node]+=diff;
 		
 		if(l==r) {
-			return tree[node]+=diff;
+			return;
 		}
 
-		long mid = (l+r)/2;
+		int mid = (l+r)/2;
 		
-		return tree[node] = updateTree(2*node,l,mid,idx,diff) + updateTree(2*node+1,mid+1,r,idx,diff);
+		updateTree(2*node,l,mid,idx,diff);
+		updateTree(2*node+1,mid+1,r,idx,diff);
 	}
 
-	private static long createTree(int node, long l, long r) {
+	private static long createTree(int node, int l, int r) {
 		if(l==r) {
 			return tree[node] = input[l];
 		}
 		
-		long mid = (l+r)/2;
+		int mid = (l+r)/2;
 		
 		return tree[node] = createTree(2*node, l, mid) + createTree(2*node+1,mid+1,r);
 	}
