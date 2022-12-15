7ef2206728bd720c4f5a20037ff9f5475d7841bc
JooEunKim <44815092+joooeunkim@users.noreply.github.com>
2022-12-15 23:34:04 +0900

    신나는 함수 실행

--- 1215/BOJ_9184_김주은.java
+++ 1215/BOJ_9184_김주은.java
@@ -1,17 +1,14 @@
-package com.bj;
-
 import java.util.*;
 import java.io.*;
 
 public class Main_BJ_9184_신나는함수실행_김주은 {
 
-	static int[][][] dp;
+	static int[][][] dp = new int[21][21][21];
 
 	public static void main(String[] args) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
 
 		while (true) {
 
@@ -22,21 +19,12 @@ public class Main_BJ_9184_신나는함수실행_김주은 {
 			int c = Integer.parseInt(st.nextToken());
 
 			if (a == -1 && b == -1 && c == -1) {
-				break;
+				return;
 			}
 
-			// dp = new int[101][101][101];
-//			for(int i=0;i<102;i++) {
-//				for(int j=0;j<102;j++) {
-//					Arrays.fill(dp[i][j], Integer.MAX_VALUE);
-//				}
-//			}
-
-			sb.append("w(").append(a).append(", ").append(b).append(", ").append(c).append(") = ").append(w(a, b, c));
-
+			System.out.println("w("+a+", "+b+", "+c+") = "+w(a,b,c));
 		}
 
-		System.out.println(sb);
 	}
 
 	private static int w(int a, int b, int c) {
@@ -45,11 +33,17 @@ public class Main_BJ_9184_신나는함수실행_김주은 {
 			return 1;
 		} else if (a > 20 || b > 20 || c > 20) {
 			return w(20, 20, 20);
-		} else if (a < b && b < c) {
-			return w(a, b, c - 1) + w(a, b - 1, c - 1) + w(a, b - 1, c);
-		} else {
-			return w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+		} 
+		
+		if(dp[a][b][c]<=0) {
+			if (a < b && b < c) {
+				return dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+			} else {
+				return dp[a][b][c] =  w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+			}
 		}
+		
+		return dp[a][b][c];
 
 	}
 
