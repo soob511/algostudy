d7e2b0029c82d4edd2f364c1725340e3554b8fd9
JooEunKim <44815092+joooeunkim@users.noreply.github.com>
2023-01-04 23:59:47 +0900

    김주은

--- 2023/0104/예산/main_2512_김주은.java
+++ 2023/0104/예산/main_2512_김주은.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	
