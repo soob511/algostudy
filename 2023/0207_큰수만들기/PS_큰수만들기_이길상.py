0b67b83cecb247877e4179d6e4688f2e34a53041
Nelfunction <oat641@gmail.com>
2023-02-07 22:17:38 +0900

    시간초과->스택응용으로 변경

--- 2023/0207_큰수만들기/PS_큰수만들기_이길상.py
+++ 2023/0207_큰수만들기/PS_큰수만들기_이길상.py
@@ -1,17 +1,12 @@
 def solution(number, k):
     answer = []
-    N = len(number)
     start = 0
-    while start+k < N:
-        max_val = "0"
-        max_idx = 0
-        for idx in range(start, start+k+1):
-            if int(number[idx]) > int(max_val):
-                max_val = number[idx]
-                max_idx = idx
-
-        k -= (max_idx-start)
-        start = max_idx+1
-        answer.append(max_val)
-        
+    for num in number:
+        while answer and k>0 and answer[-1] < num:
+            answer.pop()
+            k-=1
+        answer.append(num)
+    for _ in range(k):
+        answer.pop()
+    
     return "".join(answer)
