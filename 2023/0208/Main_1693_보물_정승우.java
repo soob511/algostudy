7efe73c257ba2951d3565863e7b26f527d6ad8ac
정승우 <tmddn1008@naver.com>
2023-02-08 23:33:06 +0900

    0208

---
+++ 2023/0208/Main_1693_보물_정승우.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_1693_보물_정승우 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        int[] b = new int[n];
+        int sum = 0;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            b[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        for (int i = 0; i < n; i++) {
+            sum += a[i] * b[n - i - 1];
+        }
+        System.out.println(sum);
+    }
+}
