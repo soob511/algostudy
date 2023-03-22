bb69a2b1f074c9cbfa685cf2f2be396d338b3925
Nelfunction <oat641@gmail.com>
2023-03-22 20:52:39 +0900

    0322

--- 2023/0322_야근지수/PS_야근지수_이길상.py
+++ 2023/0322_야근지수/PS_야근지수_이길상.py
@@ -28,3 +28,34 @@ def solution(n, works):
         area += 1
     
     return answer
+# 효율성 테스트 결과
+# 테스트 1 〉	통과 (1.30ms, 10.2MB)
+# 테스트 2 〉	통과 (1.64ms, 10.4MB)
+
+
+# heapq 사용한 코드
+import heapq
+
+def solution(n, works):
+    if sum(works) < n:
+        return 0
+    
+    pq = []
+    
+    for w in works:
+        heapq.heappush(pq, -w)
+    
+    for _ in range(n):
+        num = heapq.heappop(pq)
+        if num ==0:
+            break
+        heapq.heappush(pq, num+1)
+    
+    answer = 0
+    for w in pq:
+        answer += w**2
+ 
+    return answer
+# 효율성 테스트 결과
+# 테스트 1 〉	통과 (348.76ms, 10.2MB)
+# 테스트 2 〉	통과 (381.86ms, 10.2MB)
