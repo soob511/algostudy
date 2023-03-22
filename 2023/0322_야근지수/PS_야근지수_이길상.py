1e09a722b9db4ecdc09a7221527faf92f546706e
Nelfunction <oat641@gmail.com>
2023-03-22 20:47:12 +0900

    0322

---
+++ 2023/0322_야근지수/PS_야근지수_이길상.py
@@ -0,0 +1,32 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12927
+
+import heapq
+
+def solution(n, works):
+    if sum(works) < n:
+        return 0
+    
+    works.sort(key = lambda x: -x)
+    L = len(works)
+    
+    answer = 0
+    area = 1
+    
+    for w in works:
+        while (area == L or w > works[area]) and n >= area:
+            w -= 1
+            n -= area
+        
+        if n < area:
+            for i in range(n):
+                answer += (w-1)*(w-1)
+            for i in range(n, area):
+                answer += w*w
+            for i in range(area, L):
+                answer += works[i]*works[i]
+
+            break
+        
+        area += 1
+    
+    return answer
