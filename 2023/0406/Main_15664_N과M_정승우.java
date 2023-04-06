5d86df5f78c8423b36fa20d542325daeb43fa21c
정승우 <tmddn1008@naver.com>
2023-04-06 23:03:36 +0900

    0406

---
+++ 2023/0406/Main_15664_N과M_정승우.java
@@ -0,0 +1,56 @@
+package 백트래킹;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_15664_N과M_정승우 {
+    static int n, m;
+    static int arr[];
+    static int res[];
+    static boolean check[];
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        res = new int[m];
+        arr = new int[n];
+        check = new boolean[n];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        dfs(0,0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int idx,int cnt) {
+        if(cnt==m) {
+            for(int i=0; i<m; i++) {
+                sb.append(res[i]+" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        //[1,7,9,9]
+        int before = 0;
+        for(int i=idx; i<n; i++) {
+
+            if(before!= arr[i]) {
+
+                res[cnt] = arr[i];
+                before = arr[i];
+                dfs(i+1, cnt+1);
+            }
+
+        }
+    }
+}
