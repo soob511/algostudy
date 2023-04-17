5743764b5edbcadeba43e4c7c6bc1ca82ae593fb
이길상 <oat641@gmail.com>
2023-04-17 21:36:49 +0900

    0417

---
+++ 2023/0417_스티커모으기2/ps_스티커모으기_이길.java
@@ -0,0 +1,16 @@
+def solution(sticker):
+    N = len(sticker)
+    
+    if N == 1:
+        return sticker[0]
+    
+    DP = [[0 for _ in range(N)] for _ in range(2)]
+    
+    DP[0][0] = sticker[0]
+    DP[1][1] = sticker[1]
+    
+    for i in range(2):
+        for j in range(i+1, N):
+            DP[i][j] = max(DP[i][j-2] + sticker[j], DP[i][j-1])
+    
+    return max(DP[0][N-2], DP[1][N-1])
