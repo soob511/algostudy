3464bb7be44923729641e06e4d7d436a5a65d99d
이길상 <oat641@gmail.com>
2023-05-09 01:53:33 +0900

    0509

---
+++ 2023/0509/PS_징검다리 건너기_이길상.py
@@ -0,0 +1,24 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/64062#
+
+def solution(stones, k):
+
+    L = []
+    for idx, val in enumerate(stones):
+        L.append((val, idx+1)) 
+    L.sort()
+
+    PREV = [i-1 for i in range(len(stones)+2)]
+    NEXT = [i+1 for i in range(len(stones)+2)]
+
+    answer = 0
+    for val, idx in L:
+        if 0 < idx < len(PREV)-1:
+
+            if NEXT[idx] - PREV[idx] > k:
+                answer = val
+                break
+
+            NEXT[PREV[idx]] = NEXT[idx]
+            PREV[NEXT[idx]] = PREV[idx]
+
+    return answer
