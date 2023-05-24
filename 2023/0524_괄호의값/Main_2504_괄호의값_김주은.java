5174e6b95f83eba24d79f6e53b129d7884c997dc
JooEunKim <44815092+joooeunkim@users.noreply.github.com>
2023-05-24 21:40:28 +0900

    0524

--- 2023/0524_괄호의값/Main_2504_괄호의값_김주은.java
+++ 2023/0524_괄호의값/Main_2504_괄호의값_김주은.java
@@ -2,41 +2,27 @@ import java.util.*;
 
 public class Main_BJ_2504_괄호의값_김주은 {
 	
-	static String s;
-	static int idx=0;
-	static Stack<Character> stack;
-
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		s = sc.next();
-		stack = new Stack();
+		String s = sc.next();
+		Stack<Character> stack = new Stack();
 		
-		int answer=0;
-		int depth=0;
-		int[] val = new int[s.length()+1];
+		int depth=-1;
+		int[] val = new int[s.length()];
 		
 		for(int i=0;i<s.length();i++) {
 			char c = s.charAt(i);
 			
-			if(stack.isEmpty()) {
-				if(c=='(' || c=='[') {
-					depth+=1;
-					stack.push(c);
-				}else {
-					System.out.println(0);
-					return;
-				}
-				continue;
+			if(stack.isEmpty() && (c==']' || c==')')) {
+				System.out.println(0);
+				return;
 			}
 			
 			if(c=='(' || c=='[') {
 				depth+=1;
 				stack.push(c);
-				continue;
-			}
-			
-			if(c==')') {
+			}else if(c==')') {
 				if(stack.peek()=='(') {
 					if(val[depth+1]>0) {
 						val[depth]+=(val[depth+1]*2);
@@ -68,7 +54,7 @@ public class Main_BJ_2504_괄호의값_김주은 {
 			
 		}
 		
-		System.out.println(val[1]);
+		System.out.println(val[0]);
 	}
 
 }
