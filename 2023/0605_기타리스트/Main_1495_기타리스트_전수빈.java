79c1d136a80b550faaa862610debc636924c77b5
soob511 <102846772+soob511@users.noreply.github.com>
2023-06-05 21:24:39 +0900

    0605

---
+++ 2023/0605_기타리스트/Main_1495_기타리스트_전수빈.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int ans = -1;
+        
+        int vol[] = new int[M+1];
+        int change[] = new int[N+1];
+        
+        for(int i=0; i<N; i++) change[i] = Integer.parseInt(st.nextToken());
+        
+        for(int i=0; i<M+1; i++) vol[i] = -1;
+        
+        vol[S] = 0;
+        
+        for(int i=1; i<N+1; i++){
+			List<Integer> changer = new ArrayList<>();
+            for(int j=0; j<M+1; j++){
+                if(vol[j]==i-1){
+                    int minus = j-change[i-1];
+                    int plus = j+change[i-1];
+                    
+                    if(minus>=0){
+                        changer.add(minus);
+                    }
+                    if(plus<=M){
+                        changer.add(plus);
+                    } 
+                }
+            }
+            for(int k : changer) vol[k] = i;
+        }
+        
+        for(int i=0; i<M+1; i++){
+            if(vol[i]==N) ans = Math.max(ans, i);
+        }
+        
+        System.out.println(ans);
+      
+    }
+}
