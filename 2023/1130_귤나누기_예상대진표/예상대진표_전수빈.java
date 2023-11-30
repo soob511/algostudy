95a58b23edce47d66f75cb80d114cb27c1d978ff
soob511 <asdfls@naver.com>
2023-11-30 21:03:06 +0900

    1130

---
+++ 2023/1130_귤나누기_예상대진표/예상대진표_전수빈.java
@@ -0,0 +1,17 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+        while(true){
+            answer++;
+            a = (a/2)+(a%2);
+            b = (b/2)+(b%2);
+            if(a==b){
+                break;
+            }
+        }
+     
+        return answer;
+    }
+}
