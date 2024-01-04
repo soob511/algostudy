1084cb9ccba54227a3367ac9b3308701b6677f08
정승우 <tmddn1008@naver.com>
2024-01-04 21:16:25 +0900

    0104

---
+++ 2024/0104/PS_최댓값과최솟값_정승우.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] str = s.split(" ");
+
+        int[] arr = new int[str.length];
+
+        for(int i=0; i<str.length; i++) {
+            arr[i] = Integer.parseInt(str[i]);
+        }
+        Arrays.sort(arr);
+        answer = arr[0]+" "+arr[arr.length-1];
+
+
+
+        return answer;
+    }
+}
