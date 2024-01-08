52e7dce057d4cb80130cd083c63b1830796bbbee
이길상 <oat641@gmail.com>
2024-01-08 20:59:38 +0900

    0108

--- 2024/0108_더맵게/PS_n^2배열자르기_이길상.py
+++ 2024/0108_더맵게/PS_n^2배열자르기_이길상.py
@@ -1,5 +1,12 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/87390
+
 def solution(n, left, right):
-    answer = []
-    for i in range(left, right + 1):
-        answer.append(max(i//n + 1, i%n + 1))
-    return answer
+    return [max(i//n + 1, i%n + 1) for i in range(left, right + 1)]
+
+
+
+# def solution(n, left, right):
+#     answer = []
+#     for i in range(left, right + 1):
+#         answer.append(max(i//n + 1, i%n + 1))
+#     return answer
